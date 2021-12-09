282b92c1720a791240b574c0610709396af1ee72
John Kryspin <johnkryspin@gmail.com>
2021-12-09 08:07:52 -0500

    cleaned up

--- AdventOfCode/Tasks/2021/Day9/Solution.cs
+++ AdventOfCode/Tasks/2021/Day9/Solution.cs
@@ -10,7 +10,15 @@ namespace AdventOfCode.Tasks.Year2021.Day9
         public long Part1(string input)
         {
             var lines = input.Split("\n");
-            var heightMap = new int[lines[0].Length, lines.Length];
+            var heightMap = GetHeightMap(lines);
+
+            var points = FindLowPoints(heightMap);
+            return points.Select(point =>point.value).Sum();
+        }
+
+        private static Point[,] GetHeightMap(string[] lines)
+        {
+            var heightMap = new Point[lines[0].Length, lines.Length];
 
             var y = 0;
             foreach (var line in lines)
@@ -18,76 +26,41 @@ namespace AdventOfCode.Tasks.Year2021.Day9
                 var x = 0;
                 foreach (var height in line)
                 {
-                    // Console.WriteLine($"x {x}, y {y}");
-                    heightMap[x, y] = int.Parse(height.ToString());
+                    heightMap[x, y] = new Point
+                    {
+                        seen = false,
+                        value = int.Parse(height.ToString()),
+                        x = x,
+                        y = y
+                    };
                     x++;
                 }
 
                 y++;
             }
 
-            var points = FindLowPoints(heightMap);
-            return points.Sum();
+            return heightMap;
         }
 
-        private List<int> FindLowPoints(int[,] heightMap)
+
+        private List<Point> FindLowPoints(Point[,] heightMap)
         {
-            var lowPoints = new List<int>();
+            var lowPoints = new List<Point>();
             for (var y = 0; y < heightMap.GetLength(1); y++)
             {
                 for (var x = 0; x < heightMap.GetLength(0); x++)
                 {
-                    if (IsLowPoint(x, y, heightMap)) lowPoints.Add(heightMap[x, y]);
-                }
-            }
-
-            return lowPoints.Select(point => point + 1).ToList();
-        }
-
-        private List<(int, int)> FindLowPointsPart2(Point[,] heightMap)
-        {
-            var lowPoints = new List<(int, int)>();
-            for (var y = 0; y < heightMap.GetLength(1); y++)
-            {
-                for (var x = 0; x < heightMap.GetLength(0); x++)
-                {
-                    if (IsLowPointPart2(x, y, heightMap)) lowPoints.Add((x, y));
+                    var lowPoint = GetLowPoint(x, y, heightMap);
+                    if (lowPoint != null)
+                    {
+                        lowPoints.Add(lowPoint);
+                    }
                 }
             }
 
             return lowPoints;
         }
-
-        private bool IsLowPoint(int x, int y, int[,] heightMap)
-        {
-            var target = heightMap[x, y];
-            // for(var xDiff = -1)
-            var points = new List<int>();
-
-            //left
-            points.Add(GetPoint(x - 1, y, heightMap));
-            //right
-            points.Add(GetPoint(x + 1, y, heightMap));
-            //up
-            points.Add(GetPoint(x, y - 1, heightMap));
-            //down 
-            points.Add(GetPoint(x, y + 1, heightMap));
-            var count = points.Count(item => target < item);
-            if (count == 4) return true;
-            return false;
-        }
-
-        private int GetPoint(int x, int y, int[,] heightMap)
-        {
-            if (x > heightMap.GetUpperBound(0) || x < heightMap.GetLowerBound(0) ||
-                y > heightMap.GetUpperBound(1) || y < heightMap.GetLowerBound(1))
-            {
-                return 10;
-            }
-
-            return heightMap[x, y];
-        }
-        private int GetPointPart2(int x, int y, Point[,] heightMap)
+        private int GetPoint(int x, int y, Point[,] heightMap)
         {
             if (x > heightMap.GetUpperBound(0) || x < heightMap.GetLowerBound(0) ||
                 y > heightMap.GetUpperBound(1) || y < heightMap.GetLowerBound(1))
@@ -116,34 +89,14 @@ namespace AdventOfCode.Tasks.Year2021.Day9
         {
             {
                 var lines = input.Split("\n");
-                var heightMap = new Point[lines[0].Length, lines.Length];
-
-                var y = 0;
-                foreach (var line in lines)
-                {
-                    var x = 0;
-                    foreach (var height in line)
-                    {
-                        // Console.WriteLine($"x {x}, y {y}");
-                        heightMap[x, y] = new Point(int.Parse(height.ToString()));
-                        x++;
-                    }
-
-                    y++;
-                }
-
-                var points = FindLowPointsPart2(heightMap);
+                var heightMap = GetHeightMap(lines);
+                
+                var points = FindLowPoints(heightMap);
 
                 var basins = new List<int>();
                 foreach (var point in points)
                 {
-                    basins.Add(FindBasinLength(point.Item1, point.Item2, heightMap, heightMap[point.Item1, point.Item2].value-1));
-                }
-                
-                // return
-                foreach (var VARIABLE in basins)
-                {
-                    Console.WriteLine(VARIABLE);
+                    basins.Add(FindBasinLength(point.x, point.y, heightMap, heightMap[point.x, point.y].value-1));
                 }
                 
                 return basins.OrderByDescending(a=>a).Take(3).Aggregate(1, (a,b)=>a*b);
@@ -183,23 +136,22 @@ namespace AdventOfCode.Tasks.Year2021.Day9
                    FindBasinLength(x, y - 1, heightMap, heightMap[x, y].value) +
                    FindBasinLength(x, y + 1, heightMap, heightMap[x, y].value) + 1;
         }
-        private bool IsLowPointPart2(int x, int y, Point[,] heightMap)
+        private Point GetLowPoint(int x, int y, Point[,] heightMap)
         {
             var target = heightMap[x, y];
-            // for(var xDiff = -1)
             var points = new List<int>();
 
             //left
-            points.Add(GetPointPart2(x - 1, y, heightMap));
+            points.Add(GetPoint(x - 1, y, heightMap));
             //right
-            points.Add(GetPointPart2(x + 1, y, heightMap));
+            points.Add(GetPoint(x + 1, y, heightMap));
             //up
-            points.Add(GetPointPart2(x, y - 1, heightMap));
+            points.Add(GetPoint(x, y - 1, heightMap));
             //down 
-            points.Add(GetPointPart2(x, y + 1, heightMap));
+            points.Add(GetPoint(x, y + 1, heightMap));
             var count = points.Count(item => target.value < item);
-            if (count == 4) return true;
-            return false;
+            if (count == 4) return heightMap[x,y];
+            return null;
         }
 
     }
@@ -208,10 +160,7 @@ namespace AdventOfCode.Tasks.Year2021.Day9
     {
         public bool seen;
         public int value;
-
-        public Point(int value)
-        {
-            this.value = value;
-        }
+        public int x;
+        public int y;
     }
 }
