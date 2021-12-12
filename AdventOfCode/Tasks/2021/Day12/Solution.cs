5648935d4e7de5e0e0e3b37872a91d1a82d47a6a
John Kryspin <johnkryspin@gmail.com>
2021-12-12 09:19:23 -0500

    cleaned up

--- AdventOfCode/Tasks/2021/Day12/Solution.cs
+++ AdventOfCode/Tasks/2021/Day12/Solution.cs
@@ -9,9 +9,17 @@ namespace AdventOfCode.Tasks.Year2021.Day12
     {
         public long Part1(string input)
         {
-            return 0;
             var lines = input.Split("\n");
             var graph = new Graph();
+            InitializeGraph(lines, graph);
+
+            graph.DFS("start", "end", new List<string>(), new Dictionary<string, int>(),"NOTHING");
+            
+            return graph.paths.Count();
+        }
+
+        private static void InitializeGraph(string[] lines, Graph graph)
+        {
             foreach (var line in lines)
             {
                 var vertexes = line.Split("-");
@@ -31,57 +39,27 @@ namespace AdventOfCode.Tasks.Year2021.Day12
 
                 graph.AddAnEdge(vertexes[0], vertexes[1]);
             }
-
-            graph.DFS("start", "end", new List<string>(), new Dictionary<string, bool>());
-            
-            foreach (var item in graph.paths)
-            {
-                foreach (var d in item)
-                {
-                    Console.Write($"{d}-");
-                }
-                Console.Write("\n");
-            }
-            return graph.paths.Count();
         }
-
 
 
         public long Part2(string input)
         {
             var lines = input.Split("\n");
             var graph = new Graph();
-            foreach (var line in lines)
+            InitializeGraph(lines, graph);
+
+            foreach (var vertex in graph._adjList)
             {
-                var vertexes = line.Split("-");
-
-                graph.AddVertex(
-                    new Vertex
-                    {
-                        _name = vertexes[0],
-                        _isSmallCave = vertexes[0].All(c => !char.IsUpper(c)),
-                    });
-                graph.AddVertex(
-                    new Vertex
-                    {
-                        _name = vertexes[1],
-                        _isSmallCave = vertexes[1].All(c => !char.IsUpper(c)),
-                    });
-
-                graph.AddAnEdge(vertexes[0], vertexes[1]);
-            }
-
-            graph.DFS("start", "end", new List<string>(), new Dictionary<string, bool>());
-            
-            foreach (var item in graph.paths)
-            {
-                foreach (var d in item)
+                if (vertex._name == "start" || vertex._name == "end")
                 {
-                    Console.Write($"{d}-");
+                    continue;
                 }
-                Console.Write("\n");
+                graph.DFS("start", "end", 
+                    new List<string>(),
+                    new Dictionary<string, int>(), vertex._name);
             }
-            return graph.paths.Count();
+
+            return graph.GetUniquePaths();
         }
     }
 
@@ -95,7 +73,7 @@ namespace AdventOfCode.Tasks.Year2021.Day12
 
     class Graph
     {
-        private List<Vertex> _adjList = new List<Vertex>();
+        public List<Vertex> _adjList = new List<Vertex>();
 
         public bool AddVertex(Vertex newVertex)
         {
@@ -108,11 +86,6 @@ namespace AdventOfCode.Tasks.Year2021.Day12
             return true;
         }
 
-        public int GetNumberOfVertex()
-        {
-            return _adjList.Count();
-        }
-
         public Vertex GetVertexByName(string name)
         {
             return _adjList.Find(vertex => vertex._name == name);
@@ -120,20 +93,32 @@ namespace AdventOfCode.Tasks.Year2021.Day12
 
         public bool AddAnEdge(string vertex1, string vertex2)
         {
-            Console.WriteLine($"{vertex1}-{vertex2}");
             _adjList.Find(e=>e._name ==vertex1)!._edges.Add(vertex2);
             _adjList.Find(e=>e._name ==vertex2)!._edges.Add(vertex1);
             return true;
         }
 
         public List<List<string>> paths = new List<List<string>>();
+
+        public int GetUniquePaths()
+        {
+            var pathsString = new List<string>();
+            foreach (var path in paths)
+            {
+                  pathsString.Add(string.Join("-",path));
+            }
+
+            pathsString = pathsString.Distinct().ToList();
+            
+            return pathsString.Count();
+        }
         public void DFS(string startVertexName, string endVertexName, List<string> pathSoFar,
-         Dictionary<string, bool> visited)
+         Dictionary<string, int> visited, string specialCaveName)
         {
             var currentVertex = GetVertexByName(startVertexName);
             pathSoFar.Add(currentVertex._name);
             
-            var localVisited = new Dictionary<string, bool>(visited);
+            var localVisited = new Dictionary<string, int>(visited);
             if (currentVertex._name == endVertexName)
             {
                 paths.Add(pathSoFar);
@@ -142,13 +127,14 @@ namespace AdventOfCode.Tasks.Year2021.Day12
             {
                 localVisited.TryGetValue(currentVertex._name, out var isVisited);
                 if (!currentVertex._isSmallCave || (
-                        currentVertex._isSmallCave && !isVisited))
+                        currentVertex._isSmallCave && isVisited==0) ||
+                    currentVertex._isSmallCave && isVisited==1 && specialCaveName == currentVertex._name)
                 {
-                    localVisited[currentVertex._name] = true;
+                    localVisited[currentVertex._name] = isVisited + 1;
                     foreach (var vertex in currentVertex._edges)
                     {
                         DFS(vertex, endVertexName, new List<string>(pathSoFar),
-                            new Dictionary<string, bool>(localVisited));
+                            new Dictionary<string, int>(localVisited), specialCaveName);
                     }
                 }
             }
