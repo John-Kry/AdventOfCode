ba70953d21e28ee8e8e1aa5cc60e8e8767ba2dd7
John Kryspin <johnkryspin@gmail.com>
2021-12-14 17:21:12 -0500

    day 14 completed!

--- AdventOfCode/Tasks/2021/Day14/Solution.cs
+++ AdventOfCode/Tasks/2021/Day14/Solution.cs
@@ -15,43 +15,63 @@ namespace AdventOfCode.Tasks.Year2021.Day14
 
         private static long Solve(string input, bool part1)
         {
-            var maxSteps = part1 ? 5 : 40;
+            var maxSteps = part1 ? 10 : 40;
             var parts = input.Split("\n\n");
             var template = parts[0];
             var rules = parts[1].Split("\n");
             var ruleDict = new Dictionary<string, char>();
+            var pairCount = new Dictionary<string, long>();
             foreach (var rule in rules)
             {
                 var tempRules = rule.Split(" -> ");
                 ruleDict[tempRules[0]] = Convert.ToChar(tempRules[1]);
             }
 
-            for (var step = 0; step < maxSteps; step++)
+            for (var i = 0; i < template.Length - 1; i++)
             {
-                var temp = template;
-                // sliding window
-                var additionalItem = 0;
-                for (var i = 0; i < template.Length - 1; i++)
+                string subString = template[i].ToString() + template[i + 1].ToString();
+                if (pairCount.ContainsKey(subString))
                 {
-                    string subString = template[i].ToString() + template[i + 1].ToString();
-                    if (ruleDict.ContainsKey(subString))
-                    {
-                        temp = temp.Insert(i + 1 + additionalItem, ruleDict[subString].ToString());
-                        additionalItem++;
-                    }
-                    Console.WriteLine(i);
+                    pairCount[subString]++;
                 }
-
-                Console.WriteLine(step);
-                template = temp;
+                else
+                {
+                    pairCount[subString] = 1;
+                }
             }
 
-            var resultAsArray = template.ToList();
-            var query = resultAsArray.GroupBy(item => item).OrderByDescending(g => g.Count());
-            var most = query.First().Count();
-            var least = query.Last().Count();
-            Console.WriteLine($"most: {most}, least {least}");
-            return most - least;
+
+            for (var step = 0; step < maxSteps; step++)
+            {
+                var newPairs = new Dictionary<string, long>();
+                foreach (var (key, value) in pairCount)
+                {
+                    var insert = ruleDict[key];
+                    var p1 = $"{key[0]}{insert}";
+                    var p2 = $"{insert}{key[1]}";
+                    newPairs[p1] = newPairs.GetValueOrDefault(p1) + value;
+                    newPairs[p2] = newPairs.GetValueOrDefault(p2) + value;
+                }
+
+                pairCount = newPairs;
+
+            }
+
+            foreach (var kvp in pairCount)
+            {
+                Console.WriteLine($"{kvp.Key}, {kvp.Value}");
+            }
+
+            var letterCount = new Dictionary<char, long>();
+            foreach (var kvp in pairCount)
+            {
+                letterCount[kvp.Key[0]] = letterCount.GetValueOrDefault(kvp.Key[0]) + kvp.Value;
+                letterCount[kvp.Key[1]] = letterCount.GetValueOrDefault(kvp.Key[1]) + kvp.Value;
+            }
+            letterCount[parts[1][0]]++;
+            letterCount[parts[1].Last()]++;
+
+            return (letterCount.Values.Max() - letterCount.Values.Min()) / 2;
         }
 
         public long Part2(string input)
