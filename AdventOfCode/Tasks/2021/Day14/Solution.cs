ccd9f50d10e5de5479316f8211897b5a314ac5bd
John Kryspin <johnkryspin@gmail.com>
2021-12-14 17:41:37 -0500

    cleaned up even more

--- AdventOfCode/Tasks/2021/Day14/Solution.cs
+++ AdventOfCode/Tasks/2021/Day14/Solution.cs
@@ -10,12 +10,11 @@ namespace AdventOfCode.Tasks.Year2021.Day14
     {
         public long Part1(string input)
         {
-            return Solve(input, true);
+            return Solve(input, 10);
         }
 
-        private static long Solve(string input, bool part1)
+        private static long Solve(string input, int maxSteps)
         {
-            var maxSteps = part1 ? 10 : 40;
             var parts = input.Split("\n\n");
             var template = parts[0];
             var rules = parts[1].Split("\n");
@@ -29,17 +28,9 @@ namespace AdventOfCode.Tasks.Year2021.Day14
 
             for (var i = 0; i < template.Length - 1; i++)
             {
-                string subString = template[i].ToString() + template[i + 1].ToString();
-                if (pairCount.ContainsKey(subString))
-                {
-                    pairCount[subString]++;
-                }
-                else
-                {
-                    pairCount[subString] = 1;
-                }
+                var subString = $"{template[i]}{template[i + 1]}";
+                pairCount[subString] = pairCount.GetValueOrDefault(subString) + 1;
             }
-
 
             for (var step = 0; step < maxSteps; step++)
             {
@@ -54,29 +45,24 @@ namespace AdventOfCode.Tasks.Year2021.Day14
                 }
 
                 pairCount = newPairs;
-
-            }
-
-            foreach (var kvp in pairCount)
-            {
-                Console.WriteLine($"{kvp.Key}, {kvp.Value}");
             }
 
             var letterCount = new Dictionary<char, long>();
-            foreach (var kvp in pairCount)
+            foreach (var (key, value) in pairCount)
             {
-                letterCount[kvp.Key[0]] = letterCount.GetValueOrDefault(kvp.Key[0]) + kvp.Value;
-                letterCount[kvp.Key[1]] = letterCount.GetValueOrDefault(kvp.Key[1]) + kvp.Value;
+                letterCount[key[0]] = letterCount.GetValueOrDefault(key[0]) + value;
+                letterCount[key[1]] = letterCount.GetValueOrDefault(key[1]) + value;
             }
-            letterCount[parts[0][0]]++;
-            letterCount[parts[0].Last()]++;
+
+            letterCount[template[0]]++;
+            letterCount[template.Last()]++;
 
             return (letterCount.Values.Max() - letterCount.Values.Min()) / 2;
         }
 
         public long Part2(string input)
         {
-            return Solve(input, false);
+            return Solve(input, 40);
         }
     }
 }
