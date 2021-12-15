a88e5c2de1198fb4fc437b23a981eeb762169b51
John Kryspin <johnkryspin@gmail.com>
2021-12-15 18:33:42 -0500

    more attemps

--- AdventOfCode/Tasks/2021/Day15/Solution.cs
+++ AdventOfCode/Tasks/2021/Day15/Solution.cs
@@ -10,117 +10,34 @@ namespace AdventOfCode.Tasks.Year2021.Day15
     {
         public long Part1(string input)
         {
-            return Solve(input);
+            return Solve(input, false);
         }
-
-        private static long Solve(string input)
-        {
-            var lines = input.Split("\n");
-            var grid = new long[lines[0].Length, lines.Length];
-            var pathCost = new Dictionary<(int, int), long>();
-            var prev = new Dictionary<(int, int), long>();
-            var queue = new Dictionary<(int, int), bool>();
-            Helper.CreateTwoDArrayFromString(lines, (c, x, y) => { return long.Parse(c.ToString()); }, grid);
-
-            grid.ForEachItem((x, y) =>
-            {
-                pathCost[(x, y)] = long.MaxValue;
-                // prev[(x, y)] = null;
-                queue.TryAdd((x, y), true);
-            });
-            var source = (0, 0);
-            var destination = (grid.GetLength(0) - 1, grid.GetLength(1) - 1);
-            pathCost[source] = 1;
-            while (queue.Any())
-            {
-                var u = queue.First().Key;
-                // var u = ui.First().Key;
-                // Console.WriteLine(u);
-                if (u == destination)
-                {
-                    break;
-                }
-
-                queue.Remove(u);
-                var rr = new int[] {0, 1, 0, -1};
-                var cc = new int[] {1, 0, -1, 0};
-                for (var i = 0; i < rr.Length; i++)
-                {
-                    var (x, y) = u;
-                    y += cc[i];
-                    x += rr[i];
-                    if (!queue.GetValueOrDefault((x, y)))
-                    {
-                        continue;
-                    }
-
-                    var localVertex = (x, y);
-                    var alt = pathCost[u] + grid[x, y];
-                    if (alt < pathCost[localVertex])
-                    {
-                        pathCost[localVertex] = alt;
-                        prev[localVertex] = grid[u.Item1, u.Item2];
-                    }
-                }
-            }
-
-            return pathCost[destination] - 1;
-        }
-
 
         public long Part2(string input)
         {
-            // return 0;
-            var lines = input.Split("\n");
-            var gridStart = new long[lines[0].Length, lines.Length];
+            return Solve(input, true);
+        }
+
+        public long Solve(string input, bool duplicateBy5)
+        {
+            var rr = new int[] {0, 1, 0, -1};
+            var cc = new int[] {1, 0, -1, 0};
             var pathCost = new Dictionary<(int, int), long>();
-            var prev = new Dictionary<(int, int), long>();
-            var queue = new Dictionary<(int, int), bool>();
-            Helper.CreateTwoDArrayFromString(lines, (c, x, y) => { return long.Parse(c.ToString()); }, gridStart);
-            // gridStart.Print();
-            var grid = new long[lines[0].Length * 5, lines.Length * 5];
-            var width = lines[0].Length;
-
-            gridStart.ForEachItem((x, y) =>
-            {
-                for (var xx = 0; xx < 5; xx++)
-                {
-                    for (var yy = 0; yy < 5; yy++)
-                    {
-                        grid[x + width * xx, y + width * yy] = GetNewGridValue(gridStart, x, y, xx, yy);
-                    }
-                }
-            });
-
-            // grid.Print();
-            grid.ForEachItem((x, y) =>
-            {
-                pathCost[(x, y)] = long.MaxValue;
-                prev[(x, y)] = 0;
-                queue.TryAdd((x, y), true);
-            });
+            var queue = new Queue<(int, int)>();
+            var grid = GetGrid(input, queue, pathCost, duplicateBy5);
             var source = (0, 0);
             var destination = (grid.GetLength(0) - 1, grid.GetLength(1) - 1);
-            pathCost[source] = 1;
-            while (queue.Any())
+            pathCost[source] = 0;
+            while (queue.Count >0)
             {
                 // var u = pathCost.Min().Key;
-                var u = queue.OrderBy((pair => pathCost.GetValueOrDefault(pair.Key))).First().Key;
-
-                if (u == destination)
-                {
-                    break;
-                }
-
-                queue.Remove(u);
-                var rr = new int[] {0, 1, 0, -1};
-                var cc = new int[] {1, 0, -1, 0};
+                var u = queue.Dequeue();
                 for (var i = 0; i < rr.Length; i++)
                 {
                     var (x, y) = u;
                     y += cc[i];
                     x += rr[i];
-                    if (!queue.GetValueOrDefault((x, y)))
+                    if (!queue.Contains((x, y)))
                     {
                         continue;
                     }
@@ -130,12 +47,49 @@ namespace AdventOfCode.Tasks.Year2021.Day15
                     if (alt < pathCost[localVertex])
                     {
                         pathCost[localVertex] = alt;
-                        prev[localVertex] = grid[u.Item1, u.Item2];
                     }
                 }
             }
 
-            return pathCost[destination] - 1;
+            return pathCost[destination] - grid[source.Item1, source.Item2] + 1;
+        }
+
+        private long[,] GetGrid(string input, Queue<(int,int)> queue, Dictionary<(int, int), long> pathCost, bool duplicateBy5)
+        {
+           
+            var lines = input.Split("\n");
+            var gridStart = new long[lines[0].Length, lines.Length];
+            
+            Helper.CreateTwoDArrayFromString(lines, (c, x, y) => { return long.Parse(c.ToString()); }, gridStart);
+            // gridStart.Print();
+            long[,] grid;
+            if (duplicateBy5)
+            {
+                grid = new long[lines[0].Length * 5, lines.Length * 5]; 
+                var width = lines[0].Length;
+                gridStart.ForEachItem((x, y) =>
+                {
+                    for (var xx = 0; xx < 5; xx++)
+                    {
+                        for (var yy = 0; yy < 5; yy++)
+                        {
+                            grid[x + width * xx, y + width * yy] = GetNewGridValue(gridStart, x, y, xx, yy);
+                        }
+                    }
+                });
+            }
+            else
+            {
+                grid = gridStart;
+            }
+            
+            grid.ForEachItem((x, y) =>
+            {
+                pathCost[(x, y)] = long.MaxValue;
+                queue.Enqueue((x, y));
+            });
+            
+            return grid;
         }
 
         private static long GetNewGridValue(long[,] grid, int x, int y, int xx, int yy)
